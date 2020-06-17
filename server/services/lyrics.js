35c295472d9e8cf3dc77cba57fbea8be8a1f5cc4
Panos <anastapd@gmail.com>
2020-06-17 08:44:34 +0100

    Implemented filtering out words from songs that are not part of the song

--- server/services/lyrics.js
+++ server/services/lyrics.js
@@ -37,8 +37,17 @@ async function getWordsForSong(title, artist) {
             `https://api.lyrics.ovh/v1/${artist}/${title}`
         );
 
-        const lyrics = response.data.lyrics;
-        return lyrics.split(" ").length;
+        const lyrics = response.data.lyrics
+            .split(" ")
+            .filter(l => {
+                return !l.includes(":") && l !== "(*)" && l !== "";
+            });
+
+        if (lyrics.length === 1) {
+            console.log(lyrics)
+        }
+
+        return lyrics.length;
     } catch (error) {}
 }
 
